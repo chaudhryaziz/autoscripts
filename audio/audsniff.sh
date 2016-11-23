71a3a242e158a7bc60c17e9fd8017bd6e572e84a
Aziz Chaudhry <chaudhryaziz@gmail.com>
2016-11-22 22:44:41 -0500

    Adding prerequisites to Audio sniff n' Grab UNIX

--- audio/audsniff.sh
+++ audio/audsniff.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 #This is the Unix version of Audio Sniff n'Grab
+
+echo >&2 "Checking Prerequisites";
+command -v ssh >/dev/null 2>&1 || { echo >&2 "I require ssh but it's not installed."; }
+command -v lftp >/dev/null 2>&1 || { echo >&2 "I require lftp but it's not installed. Aborting."; exit 1;}
+#command -v python >/dev/null 2>&1 || { echo >&2 "I require lftp but it's not installed. Aborting."; exit 1;}
